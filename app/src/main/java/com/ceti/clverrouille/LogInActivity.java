ed26ffba794ddd974bb21fdb6f2dd34a35fcdf8c
ramoncinp <ramon.parra@chipred.com>
2019-06-06 08:36:29 -0500

    Agregar dialogo con progress bar

--- app/src/main/java/com/ceti/clverrouille/LogInActivity.java
+++ app/src/main/java/com/ceti/clverrouille/LogInActivity.java
@@ -1,15 +1,35 @@
 package com.ceti.clverrouille;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity
 {
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        //Inicializar Views
+        initViews();
+    }
+
+    private void initViews()
+    {
+        Button logInButton = findViewById(R.id.log_in_button);
+        logInButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Constantes.showProgressDialog(LogInActivity.this, alertDialog);
+            }
+        });
     }
 }
