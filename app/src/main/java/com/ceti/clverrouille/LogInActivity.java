62375cecc2940d758c6524a1f6711e72d190ba8d
ramoncinp <ramon.parra@chipred.com>
2019-06-08 13:01:43 -0500

    Crear query para obtener cliente por correo electrónico.

--- app/src/main/java/com/ceti/clverrouille/LogInActivity.java
+++ app/src/main/java/com/ceti/clverrouille/LogInActivity.java
@@ -1,14 +1,23 @@
 package com.ceti.clverrouille;
 
 import android.app.AlertDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class LogInActivity extends AppCompatActivity
 {
     private AlertDialog alertDialog;
+    private DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,6 +38,26 @@ public class LogInActivity extends AppCompatActivity
             public void onClick(View v)
             {
                 Constantes.showProgressDialog(LogInActivity.this, alertDialog);
+            }
+        });
+    }
+
+    private void getUser(String email)
+    {
+        users = FirebaseDatabase.getInstance().getReference("usuarios");
+        Query query = users.orderByChild("email").equalTo(email);
+        query.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
             }
         });
     }
